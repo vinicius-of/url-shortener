1a0d1e5c1479ac0e5a2d7cf8834456060accc2b5
Vinicius Fonseca <vinicius.oliveirafonseca@gmail.com>
2025-06-28 20:07:17 -0300

    fix(removing): Removing gateway app from project

--- apps/gateway/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { GatewayModule } from './gateway.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(GatewayModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
