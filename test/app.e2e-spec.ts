7539ee448873a51e4a312911a5a869bff6392494
Vinicius Fonseca <vinicius.oliveirafonseca@gmail.com>
2025-06-29 18:17:27 -0300

    misc

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,21 +4,18 @@ import * as request from 'supertest';
 import { App } from 'supertest/types';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+    let app: INestApplication<App>;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [],
-    }).compile();
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [],
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+    it('/ (GET)', () => {
+        return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+    });
 });
